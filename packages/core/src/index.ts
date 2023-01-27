3353f5ec3696b1c6f6ed08d72bddb2bbec37442a
Shigma <shigma10826@gmail.com>
2023-01-28 00:12:32 +0800

    feat(core): add core implementation

---
+++ packages/core/src/index.ts
@@ -0,0 +1,59 @@
+import { Context, Service } from 'koishi'
+
+declare module 'koishi' {
+  interface Context {
+    __cache__: Cache
+    cache<K extends keyof Tables>(key: K): CacheTable<Tables[K]>
+  }
+}
+
+export interface Tables {
+  default: any
+}
+
+class CacheTable<T> {
+  constructor(public ctx: Context, public table: keyof Tables, public config: CacheTable.Config = {}) {}
+
+  clear() {
+    return this.ctx.__cache__.clear(this.table)
+  }
+
+  get(key: string): Promise<T> {
+    return this.ctx.__cache__.get(this.table, key)
+  }
+
+  set(key: string, value: T, maxAge = this.config.maxAge) {
+    return this.ctx.__cache__.set(this.table, key, value, maxAge)
+  }
+
+  delete(key: string) {
+    return this.ctx.__cache__.delete(this.table, key)
+  }
+}
+
+namespace CacheTable {
+  export interface Config {
+    maxAge?: number
+  }
+}
+
+abstract class Cache extends Service {
+  constructor(ctx: Context) {
+    super(ctx, 'cache')
+  }
+
+  public cache<K extends keyof Tables>(table?: K, config?: CacheTable.Config): CacheTable<Tables[K]> {
+    return new CacheTable(this.ctx, table || 'default', config)
+  }
+
+  abstract clear(table: string): Promise<void>
+  abstract get(table: string, key: string): Promise<any>
+  abstract set(table: string, key: string, value: any, maxAge?: number): Promise<void>
+  abstract delete(table: string, key: string): Promise<void>
+}
+
+Context.service('__cache__', {
+  methods: ['cache'],
+})
+
+export default Cache
