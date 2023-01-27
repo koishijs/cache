0d9bce74a76e9393de8f464fe8bc26d03d1a3425
Shigma <shigma10826@gmail.com>
2023-01-28 00:28:24 +0800

    feat(database): support database cache

---
+++ packages/database/README.md
@@ -0,0 +1,9 @@
+# [koishi-plugin-cache-database](https://cache.koishi.chat/plugins/database.html)
+ 
+[![npm](https://img.shields.io/npm/v/koishi-plugin-cache-database?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-cache-database)
+
+基于数据库的缓存服务。
+
+## 文档
+
+<https://cache.koishi.chat/plugins/database.html>
---
+++ packages/database/src/index.ts
@@ -0,0 +1,81 @@
+import { Context, Schema, Time } from 'koishi'
+import Cache from '@koishijs/cache'
+
+declare module 'koishi' {
+  interface Tables {
+    cache: CacheEntry
+  }
+}
+
+interface CacheEntry {
+  table: string
+  key: string
+  value: string
+  expire: Date
+}
+
+class DatabaseCache extends Cache {
+  static using = ['database'] as const
+
+  constructor(ctx: Context, private config: DatabaseCache.Config) {
+    super(ctx)
+
+    ctx.model.extend('cache', {
+      table: 'string(63)',
+      key: 'string(63)',
+      value: 'string',
+      expire: 'timestamp',
+    }, {
+      primary: ['table', 'key'],
+    })
+
+    ctx.setInterval(async () => {
+      await ctx.database.remove('cache', { expire: { $lt: new Date() } })
+    }, config.refreshInterval)
+  }
+
+  private encode(data: any): string {
+    return JSON.stringify(data)
+  }
+
+  private decode(record: string): any {
+    return JSON.parse(record)
+  }
+
+  async clear(table: string) {
+    await this.ctx.database.remove('cache', { table })
+  }
+
+  async get(table: string, key: string) {
+    const [entry] = await this.ctx.database.get('cache', { table, key }, ['expire', 'value'])
+    if (!entry) return
+    if (entry.expire && +entry.expire < Date.now()) return
+    return this.decode(entry.value)
+  }
+
+  async set(table: string, key: string, value: any, maxAge?: number) {
+    const expire = maxAge ? new Date(Date.now() + maxAge) : null
+    await this.ctx.database.upsert('cache', [{
+      table,
+      key,
+      value: this.encode(value),
+      expire,
+    }])
+  }
+
+  async delete(table: string, key: string) {
+    await this.ctx.database.remove('cache', { table, key })
+  }
+}
+
+namespace DatabaseCache {
+  export interface Config {
+    refreshInterval?: number
+  }
+
+  export const Config: Schema<Config> = Schema.object({
+    refreshInterval: Schema.number().default(Time.minute * 10).role('time').description('缓存刷新间隔。'),
+  })
+}
+
+export default DatabaseCache
