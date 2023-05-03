bbaaae527ecf71ceb798aa19638d02e092e04854
Shigma <shigma10826@gmail.com>
2023-05-04 01:42:07 +0800

    docs: setup i18n docs

---
+++ docs/.vitepress/config/index.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@koishijs/vitepress'
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: '@koishijs/cache',
+  description: 'Koishi 缓存服务',
+
+  locales: {
+    'zh-CN': require('./zh-CN'),
+  },
+
+  themeConfig: {
+    indexName: 'cache',
+    socialLinks: {
+      github: 'https://github.com/koishijs/cache',
+    },
+  },
+})
