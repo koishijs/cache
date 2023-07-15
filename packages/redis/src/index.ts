a1a174301a12dbe74ab3323f1c828923d5062004
xcl <18436@xlc>
2023-07-15 22:41:29 +0800

    fix(redis): add url field to create redis client

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -19,6 +19,7 @@ class RedisCache extends Cache {
 
   constructor(ctx: Context, private config: RedisCache.Config) {
     super(ctx)
+    this.config.url = this.config.endpoint
   }
 
   private getRedisKey(table: string, key: string) {
